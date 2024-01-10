575be47aa4a3e072540a75a3791abb2918cebfa5
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-10 13:12:23 +0400

    implement useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import ClassCounter from './components/ClassCounter'
+import HookCounter from './components/HookCounter'
 
 function App() {
 
   return (
     <>
+    <ClassCounter />
+    <HookCounter />
     </>
   )
 }
