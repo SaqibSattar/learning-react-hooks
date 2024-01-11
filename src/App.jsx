68f9200612fc48336d18890c4649d2b21bd02bf2
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-11 10:47:46 +0400

    implement useState with array

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import ClassCounter from "./components/ClassCounter";
-import ClassCounterTwo from "./components/ClassCounterTwo";
 import HookCounter from "./components/HookCounter";
-import HookCounterThree from "./components/HookCounterThree";
+import ClassCounterTwo from "./components/ClassCounterTwo";
 import HookCounterTwo from "./components/HookCounterTwo";
+import HookCounterThree from "./components/HookCounterThree";
+import HookCounterFour from "./components/HookCounterFour";
 
 function App() {
   return (
     <>
-    <HookCounterThree />
-      {/* <ClassCounterTwo /> 
+    <HookCounterFour />
+    {/* <HookCounterThree />
+      <ClassCounterTwo /> 
       <HookCounterTwo />
       <HookCounter />
       <ClassCounter /> */}
