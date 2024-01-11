0d69474c4e5a35aea158e4a658109b3337436de8
SaqibSattar <saqibkhanz1234@gmail.com>
2024-01-11 12:09:26 +0400

    implement Conditionally run effects

--- src/components/ClassCounterOne.jsx
+++ src/components/ClassCounterOne.jsx
@@ -5,7 +5,8 @@ class ClassCounterOne extends Component {
     super(props);
 
     this.state = {
-      count: 0
+      count: 0,
+      name: "",
     };
   }
 
@@ -13,14 +14,21 @@ class ClassCounterOne extends Component {
     document.title = `You clicked ${this.state.count} times.`;
   }
 
-  componentDidUpdate(prevState, prevProps) {
-    document.title = `You clicked ${this.state.count} times.`
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      console.log("componentDidUpdate Called ");
+      document.title = `You clicked ${this.state.count} times.`;
+    }
   }
 
   render() {
     const { count } = this.state;
     return (
       <div>
+        <input
+          type="text"
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
           Count {count}
         </button>
--- src/components/HookCounterOne.jsx
+++ src/components/HookCounterOne.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 
 const HookCounterOne = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
 
   useEffect(() => {
+    console.log("HookCounterOne useEffect Called");
     document.title = `You clicked ${count} times.`;
-  });
+  }, [count]);
 
   return (
     <div>
+    <input type="text" value={name} onChange={(e) => {setName(e.target.value)}}/>
       <button onClick={() => setCount(count + 1)}>
         Count {count}
       </button>
